bc25ab815c8b4113c08138922baab920948ac171
yagocamps <133058353+yagocamps@users.noreply.github.com>
2026-09-04 00:27:49 -0300

    refactor: remove unused quadratic graph pilot

--- site/src/components/aulas/AulaView.tsx
+++ site/src/components/aulas/AulaView.tsx
@@ -7,7 +7,6 @@ import { AulaTocMobile } from "@/components/aulas/AulaTocMobile";
 import { FormulaBlock } from "@/components/aulas/FormulaBlock";
 import { FunctionPlot } from "@/components/aulas/FunctionPlot";
 import { InteractiveAfimPlot } from "@/components/aulas/InteractiveAfimPlot";
-import { InteractiveQuadraticPlot } from "@/components/aulas/InteractiveQuadraticPlot";
 import { BhaskaraDerivation } from "@/components/aulas/BhaskaraDerivation";
 import { LessonAnalytics } from "@/components/aulas/LessonAnalytics";
 import { MarkCompleteButton } from "@/components/aulas/MarkCompleteButton";
@@ -185,14 +184,6 @@ export function AulaView({
                   <InteractiveAfimPlot
                     initialA={content.grafico.interactive.a}
                     initialB={content.grafico.interactive.b}
-                    xDomain={content.grafico.xDomain}
-                    yDomain={content.grafico.yDomain}
-                  />
-                ) : content.grafico.interactive?.type === "quadratica" ? (
-                  <InteractiveQuadraticPlot
-                    initialA={content.grafico.interactive.a}
-                    initialB={content.grafico.interactive.b}
-                    initialC={content.grafico.interactive.c}
                     xDomain={content.grafico.xDomain}
                     yDomain={content.grafico.yDomain}
                   />
